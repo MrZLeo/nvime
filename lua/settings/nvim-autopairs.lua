f51bb2af7eb4bdef1e8668666a22f6559a8498cb
mrzleo <mr.zleo10@gmail.com>
2022-09-04 10:55:27 +0800

    fix autopairs enter double quote

--- lua/settings/nvim-autopairs.lua
+++ lua/settings/nvim-autopairs.lua
@@ -1,9 +1,4 @@
-require("nvim-autopairs").setup({})
-
--- If you want insert `(` after select function or method item
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on(
-    'confirm_done',
-    cmp_autopairs.on_confirm_done()
-)
+require("nvim-autopairs").setup({
+    map_cr = true,
+    map_complete = false
+})
