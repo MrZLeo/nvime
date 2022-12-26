48e2fa85a4806127984e0de566a3780a1a1ae15e
mrzleo <mr.zleo10@gmail.com>
2022-12-26 16:54:17 +0800

    fix(rust-tools): a bug from rust-analyzer

--- lua/lsp/rust.lua
+++ lua/lsp/rust.lua
@@ -1,47 +1,47 @@
 local opts = {
-    tools = { -- rust-tools options
-        autoSetHints = true,
-        reload_workspace_from_cargo_toml = true,
-        executor = require("rust-tools/executors").termopen,
-        runnables = {
-            use_telescope = true
-        },
-        inlay_hints = {
-            -- automatically set inlay hints (type hints)
-            -- default: true
-            auto = true,
-
-            -- Only show inlay hints for the current line
-            only_current_line = false,
-
-            -- whether to show parameter hints with the inlay hints or not
-            -- default: true
-            show_parameter_hints = true,
-
-            -- prefix for parameter hints
-            -- default: "<-"
-            parameter_hints_prefix = "<- ",
-
-            -- prefix for all the other hints (type, chaining)
-            -- default: "=>"
-            other_hints_prefix = "=> ",
-
-            -- whether to align to the length of the longest line in the file
-            max_len_align = false,
-
-            -- padding from the left if max_len_align is true
-            max_len_align_padding = 1,
-
-            -- whether to align to the extreme right or not
-            right_align = false,
-
-            -- padding from the right if right_align is true
-            right_align_padding = 7,
-
-            -- The color of the hints
-            highlight = "Comment",
-        },
-    },
+    -- tools = { -- rust-tools options
+    --     autoSetHints = true,
+    --     reload_workspace_from_cargo_toml = true,
+    --     executor = require("rust-tools/executors").termopen,
+    --     runnables = {
+    --         use_telescope = true
+    --     },
+    --     inlay_hints = {
+    --         -- automatically set inlay hints (type hints)
+    --         -- default: true
+    --         auto = true,
+    --
+    --         -- Only show inlay hints for the current line
+    --         only_current_line = false,
+    --
+    --         -- whether to show parameter hints with the inlay hints or not
+    --         -- default: true
+    --         show_parameter_hints = true,
+    --
+    --         -- prefix for parameter hints
+    --         -- default: "<-"
+    --         parameter_hints_prefix = "<- ",
+    --
+    --         -- prefix for all the other hints (type, chaining)
+    --         -- default: "=>"
+    --         other_hints_prefix = "=> ",
+    --
+    --         -- whether to align to the length of the longest line in the file
+    --         max_len_align = false,
+    --
+    --         -- padding from the left if max_len_align is true
+    --         max_len_align_padding = 1,
+    --
+    --         -- whether to align to the extreme right or not
+    --         right_align = false,
+    --
+    --         -- padding from the right if right_align is true
+    --         right_align_padding = 7,
+    --
+    --         -- The color of the hints
+    --         highlight = "Comment",
+    --     },
+    -- },
 
     -- all the opts to send to nvim-lspconfig
     -- these override the defaults set by rust-tools.nvim
@@ -61,6 +61,7 @@ local opts = {
                     command = "clippy",
                     allTargets = false
                 },
+                inlayHints = { locationLinks = false },
             }
         },
     },
