0cc70960b323eaebe7620be1fe67351bf67efb00
Zhang Jing <mrzleo@foxmail.com>
2023-07-29 19:27:41 +0800

    fix script if root

--- arch-install.sh
+++ arch-install.sh
@@ -1,12 +1,17 @@
-#! /bin/sh
+#! /usr/bin/bash
 
 echo "This script will help you install dependencies and install plugins automatically
 
     - Firstly, make sure your neovim version >= 0.9
-    - Password and `sudo` is required since we need to install packages
-    - Use `:q` to exit nvim once everything finished"
+    - Password and sudo is required since we need to install packages
+    - Use :q to exit nvim once everything finished
+
+    "
 
 # dependencies
-sudo pacman -S --needed python3 python-neovim nodejs ripgrep tree-sitter gcc 
+if (whoami != root)
+    then sudo pacman -S --needed python3 python-neovim nodejs ripgrep tree-sitter gcc
+    else pacman -S --needed python3 python-neovim nodejs ripgrep tree-sitter gcc
+fi
 
 nvim # open and auto install plugins
