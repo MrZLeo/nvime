50d676df9b96eb8b471f140574ace664c95aab3b
Zhang Jing <mrzleo@foxmail.com>
2024-01-24 16:49:03 +0800

    fix: global export

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,7 +1,15 @@
----@diagnostic disable:unused-local
 ---@diagnostic disable:unused-function
+---@diagnostic disable:lowercase-global
 
-local function delete_buffers_over_x(x)
+--- Delete buffers number over x
+--
+---@param x integer buffer number
+function delete_buffer_over_x(x)
+    local buf_begin = 1
+    if x < buf_begin then
+        vim.notify("x should bigger than 0")
+    end
+
     local buffers = vim.api.nvim_list_bufs()
     for _, buf in ipairs(buffers) do
         if vim.api.nvim_buf_get_number(buf) > x then
