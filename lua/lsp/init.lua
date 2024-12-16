7e931b95673c667ea2c13faf8c6de9aa46b29313
Zhang, Jing <mrzleo@sjtu.edu.cn>
2024-12-17 01:26:26 +0800

    fix: improve format settings

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -4,35 +4,52 @@
 require("lsp.settings")
 require("lsp.rust")
 
--- auto format
-local pattern = {
-    "*.rs",
-    -- "*.c",
-    -- "*.h",
-    -- "*.cpp",
-    -- "*.hpp",
-    -- "*.cc",
-    "*.lua",
-    "*.toml",
-    "*.yaml",
-    "*.json",
-    "*.go",
-    "*.build"
-}
+-- DON'T auto format
+local skip_file_type = { "c", "cpp" }
 
-vim.api.nvim_create_autocmd(
-    { "BufWritePre" },
-    {
-        pattern = pattern,
-        command = "lua vim.lsp.buf.format()"
-    }
-)
+local function is_skip_format(filetype)
+    for _, pattern in ipairs(skip_file_type) do
+        if filetype == pattern then
+            return true
+        end
+    end
+    return false
+end
 
-vim.keymap.set('v', 'ff',
-    function()
-        vim.lsp.buf.format({ range = { start = vim.fn.getpos("'<"), ['end'] = vim.fn.getpos("'>") } })
-        vim.api.nvim_input('<Esc>') -- Exit visual mode
-    end, {})
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then return end
+        if not client.supports_method('textDocument/formatting') then return end
 
+
+        if not is_skip_format(vim.bo.filetype) then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({
+                        bufnr = args.buf,
+                        id = client.id,
+                        timeout_ms = 2000,
+                    })
+                end
+            })
+        end
+
+        vim.keymap.set('v', 'ff',
+            function()
+                vim.lsp.buf.format({
+                    range = {
+                        vim.fn.getpos("'<"),
+                        vim.fn.getpos("'>")
+                    }
+                })
+                -- Exit visual mode
+                vim.api.nvim_input('<Esc>')
+            end,
+            { silent = true }
+        )
+    end
+})
 
 vim.cmd('hi link LspInlayHint Comment')
