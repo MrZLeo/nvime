ac42ae314a0163dad80540606489e3d8daf399d3
Zhang, Jing <mrzleo@sjtu.edu.cn>
2025-04-02 21:06:50 +0800

    cleanup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -46,18 +46,6 @@ local option = {
 
 -- list of plugins
 local plugins = {
-    -- Make lua_ls knows about neovim
-    {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-            library = {
-                -- See the configuration section for more details
-                -- Load luvit types when the `vim.uv` word is found
-                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-            },
-        },
-    },
     -- surrounding
     {
         'kylechui/nvim-surround',
@@ -448,19 +436,6 @@ local plugins = {
         ---@type CsvView.Options
         opts = {
             parser = { comments = { "#", "//" } },
-            keymaps = {
-                -- Text objects for selecting fields
-                textobject_field_inner = { "if", mode = { "o", "x" } },
-                textobject_field_outer = { "af", mode = { "o", "x" } },
-                -- Excel-like navigation:
-                -- Use <Tab> and <S-Tab> to move horizontally between fields.
-                -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
-                -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
-                jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
-                jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
-                jump_next_row = { "<Enter>", mode = { "n", "v" } },
-                jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
-            },
         },
         cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
         ft = { "csv" },
