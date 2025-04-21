cfa39081504fd79d41b413c92e69508f71a97b63
Zhang, Jing <mrzleo@sjtu.edu.cn>
2025-04-21 15:02:38 +0800

    simple refactor of lazynvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,48 +1,26 @@
--- Automatically install lazy.nvim
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({
         "git",
         "clone",
         "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        "--branch=stable",
+        lazyrepo,
         lazypath,
     })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Use a protected call so we don't error out on first use
-local status_ok, lazy = pcall(require, "lazy")
-if not status_ok then
-    return
-end
-
--- Lazy option
-local option = {
-    performance = {
-        cache = {
-            enabled = true,
-        },
-        reset_packpath = true, -- reset the package path to improve startup time
-        rtp = {
-            reset = true,      -- reset the runtime path to $VIMRUNTIME and your config directory
-            ---@type string[]
-            paths = {},        -- add any custom paths here that you want to includes in the rtp
-            ---@type string[] list any plugins you want to disable here
-            disabled_plugins = {
-                "gzip",
-                "matchit",
-                "matchparen",
-                "netrwPlugin",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-            },
-        },
-    },
-}
 
 -- list of plugins
 local plugins = {
@@ -403,13 +381,6 @@ local plugins = {
             },
         },
     },
-    -- im-select
-    {
-        "keaising/im-select.nvim",
-        config = function()
-            require("im_select").setup({})
-        end,
-    },
     -- comments
     {
         'numToStr/Comment.nvim',
@@ -428,4 +399,8 @@ local plugins = {
     }
 }
 
-lazy.setup(plugins, option)
+require("lazy").setup({
+    spec = plugins,
+    -- automatically check for plugin updates
+    checker = { enabled = true }
+})
