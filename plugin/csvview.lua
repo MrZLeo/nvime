eec48fb01886501f6e22377961982d56f168d921
MrZLeo <mrzleo@sjtu.edu.cn>
2026-04-07 15:33:27 +0800

    perf: csv on demand

--- plugin/csvview.lua
+++ plugin/csvview.lua
@@ -1,5 +1,13 @@
-vim.pack.add({ "https://github.com/hat0uma/csvview.nvim" })
+vim.api.nvim_create_autocmd("FileType", {
+    once = true,
+    pattern = { "csv", "tsv" },
+    callback = function()
+        vim.pack.add({
+            "https://github.com/hat0uma/csvview.nvim",
+        })
 
-require("csvview").setup({
-    parser = { comments = { "#", "//" } },
+        require("csvview").setup({
+            parser = { comments = { "#", "//" } },
+        })
+    end,
 })
